05fd33be882a7b44badde08e4f516838d9ad0735
Dave G <davegutz@alum.mit.edu>
2025-01-14 06:17:55 -0500

    Update EyePatch.py

--- pySleepyHead/EyePatch.py
+++ pySleepyHead/EyePatch.py
@@ -16,12 +16,23 @@
 """
 Filter data and try candidate filters to detect sleepiness.
 """
+from TFDelay import TFDelay
+from myFilters import LagTustin, LagExp, General2Pole, RateLimit, SlidingDeadband, TustinIntegrator, RateLagExp
+
+class Device:
+    # Logic constants
+    NOMINAL_DT = 0.1  #  From CONTROL_DELAY in SleepyHead (0.1)
+    CLOSED_S = 1.0  # Voltage trip set persistence, s ()
+    CLOSED_R = 0.5  # Voltage trip reset persistence, s ()
+
 
 class EyePatch:
     """Container of candidate filters"""
 
-    def __init__(self, data):
+    def __init__(self, data, dt=0.1):
         self.data = data
+        self.voltFilter = General2Pole(Device.NOMINAL_DT, 0.1, 0.9, -5., 5.)  # actual dt provided at run time
+        self.voltTripConf = TFDelay(False, Device.CLOSED_S, Device.CLOSED_R, Device.NOMINAL_DT)
 
     def calculate(self, init_time=-4.):
         """Filter data set and calculate candidate filter"""
